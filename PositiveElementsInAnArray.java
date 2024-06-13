9cb5616081c62e88c8f06997069c7adf6ce90009
Gouse.C <140190450+Gouse-c@users.noreply.github.com>
2024-06-13 20:03:07 +0530

    Add files via upload

---
+++ PositiveElementsInAnArray.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class PositiveElementsInAnArray
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of the array : ");
+		int n = sc.nextInt();
+		int a[]=new int[n];
+		System.out.println("Enter the array elements : ");
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		System.out.println("The positive elements in the array is : ");
+		for(int i=0;i<n;i++)
+		{
+			if(a[i]>0)
+			{
+				System.out.println(a[i]+" ");
+			}
+		}
+	}
+}
