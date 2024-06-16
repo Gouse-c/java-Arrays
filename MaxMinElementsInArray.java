1e29075e33be383ae28cdf539a1677b59131e0f6
Gouse.C <140190450+Gouse-c@users.noreply.github.com>
2024-06-16 18:51:14 +0530

    Add files via upload

---
+++ MaxMinElementsInArray.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class MaxMinElementsInArray
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of the array : ");
+		int n = sc.nextInt();
+		int a[] = new int[n];
+		System.out.println("Enter the array elements : ");
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		int max=a[0];
+		int min=a[0];
+		for(int i=0;i<n;i++)
+		{
+			if(a[i]>max)
+			{
+				max=a[i];
+			}
+			if(a[i]<min)
+			{
+				min=a[i];
+			}
+		}
+		System.out.println("The Maximum element is : "+max);
+		System.out.println("The Minimum element is : "+min);
+	}
+}
