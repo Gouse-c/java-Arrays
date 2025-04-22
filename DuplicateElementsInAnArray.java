e4d06e04c6d349125b430197e63bec0e488517b9
Gouse.C <140190450+Gouse-c@users.noreply.github.com>
2025-04-22 23:31:36 +0530

    Add files via upload

--- DuplicateElementsInAnArray.java
+++ DuplicateElementsInAnArray.java
@@ -1,39 +1,18 @@
 import java.util.Scanner;
-class DuplicateElementsInAnArray
-{
-	public static void main(String args[])
-	{
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the size of the array : ");
-		int n=sc.nextInt();
-		int a[]=new int[n];
-		System.out.println("Enter the array elements : ");
-		for(int i=0;i<=a.length-1;i++)
-		{
-			a[i]=sc.nextInt();
-		}
-		System.out.println("\nDuplicates are :");
-		for(int i=0;i<=a.length-1;i++)
-		{
-			int target=a[i];
-			int count=0;
-			if(a[i]==-1)
-			{
-				continue;
-			}
-			for(int j=i+1;j<=a.length-1;j++)
-			{
-				if(target==a[j])
-				{
-					count++;
-					a[j]=-1;
-				}
-				
-			}
-			if(count>0)
-				{
-					System.out.println("\n"+a[i]);
-				}
-		}
-	}
+public class DuplicateElementsInAnArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),arr[]=new int[n];
+        for(int i=0;i<n;i++) arr[i]=sc.nextInt();
+        System.out.println("Duplicates:");
+        for(int i=0;i<n;i++) {
+            for(int j=i+1;j<n;j++) {
+                if(arr[i]==arr[j]) {
+                    System.out.print(arr[i]+" ");
+                    break;
+                }
+            }
+        }
+        sc.close();
+    }
 }
